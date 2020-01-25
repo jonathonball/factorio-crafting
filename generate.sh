302c2253306e316fd501726be5c7b51c558c31b1
Jon Ball <jonathon.ball@gmail.com>
2020-01-25 15:33:59 -0500

    Updated generate.sh to support a different dir structure

--- generate.sh
+++ generate.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-for format in svg png; do
-    for file in *.dot; do
-        dot -T $format -O $file;
+error_message="Usage:\t$(basename $0) VERSION";
+
+if [ -z "$1" ]; then
+    echo -e "$error_message";
+    exit 1;
+fi;
+
+if ! [ -d "./${1}" ]; then
+    echo -e "$error_message";
+    exit 1; 
+else
+    cd $1;
+    for format in svg png; do
+        for file in *.dot; do
+            dot -T $format -O $file;
+        done;
     done;
-done;
+fi;
+
